15150c77ef4b4061b6880b9b3db07a23ebc48121
DCook18 <danieladam.cook@icloud.com>
2025-05-22 15:42:16 +0100

    Getting access to device camera

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ import cv2
 if __name__ == "__main__":
     cam = cv2.VideoCapture(0)
     
+    frame_width = int(cam.get(cv2.CAP_PROP_FRAM_WIDTH))
+    frame_height = int(cam.get(cv2.CAP_PROP_FRAM_WIDTH))
