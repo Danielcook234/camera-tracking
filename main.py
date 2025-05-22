27b6a6e20c895a8684cc7bffa59de61af3101846
DCook18 <danieladam.cook@icloud.com>
2025-05-22 15:50:29 +0100

    will show camera output on device screen

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import cv2
 
 if __name__ == "__main__":
+    #open camera
     cam = cv2.VideoCapture(0)
     
-    frame_width = int(cam.get(cv2.CAP_PROP_FRAM_WIDTH))
-    frame_height = int(cam.get(cv2.CAP_PROP_FRAM_WIDTH))
+    #get width and height
+    frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+    fourcc = cv2.VideoWriter_fourcc(*'mpv4')
+    out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width,frame_height))
+
+    while True:
+        ret, frame = cam.read()
+
+        #write frame to output file
+        out.write(frame)
+
+        #display captured frame
+        cv2.imshow('Camera',frame)
+
+        #press q to exit the loop
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cam.release()
+    out.release()
+    cv2.destroyAllWindows()
